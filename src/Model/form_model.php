e343c55611f128e12bb2641521b72d65610bbf11
Dairis <dairis.apinis@gmail.com>
2023-08-03 18:08:32 +0300

    Create the deleteData method for form_model.php

--- src/Model/form_model.php
+++ src/Model/form_model.php
@@ -23,8 +23,20 @@ class form_model
         $statement->execute();
     }
 
-    public function deleteData()
+    public function deleteData($id)
     {
+        // Create a connection to the Database
+        $database = new Database();
+        $pdo = $database->pdo();
+
+        // Prepare the SQL query using a placeholder for id
+        $sql = "DELETE FROM lists WHERE id = :id";
+        $statement = $pdo->prepare($sql);
+
+        // Bind the parameter to the placeholder of id
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+        $statement->execute();
     }
 
     public function editData()
