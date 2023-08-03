bab37defdbc1fa7dcfd57d988e9fd2ffbf89b4ee
Dairis <dairis.apinis@gmail.com>
2023-08-03 18:31:18 +0300

    Created a editData method in the form_model.php

--- src/Model/form_model.php
+++ src/Model/form_model.php
@@ -39,8 +39,22 @@ class form_model
         $statement->execute();
     }
 
-    public function editData()
+    public function editData($id, $title, $description)
     {
+         // Create a connection to the Database
+         $database = new Database();
+         $pdo = $database->pdo();
+
+         // Prepare the SQL query using placeholders for values
+         $sql ="UPDATE lists SET title = :title, description = :description WHERE id =:id";
+         $statement = $pdo->prepare($sql);
+
+          // Bind the parameter to the placeholder od respective variables
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->bindParam(':title', $title, PDO::PARAM_STR);
+        $statement->bindParam(':description', $description, PDO::PARAM_STR);
+
+        $statement->execute();
     }
 
     public function displayData()
