00260861457fff5ad45b76cee8b41c58cdf8c853
Dairis <dairis.apinis@gmail.com>
2023-08-04 17:28:51 +0300

    Improved the index_view.php method to display the index page, but did not finish it

--- src/view/index_view.php
+++ src/view/index_view.php
@@ -1,25 +1,34 @@
 <?php
 class IndexView {
+  
     public function renderItems($items) {
         // HTML template for displaying database information
         // Same content as index.html, but within a PHP method
-        echo "<!DOCTYPE html>
-        <html>
-        <head>
-            <title>My Database Information</title>
-        </head>
-        <body>
-            <h1>Database Information</h1>
-            <ul>";
+
+            echo '<div class="flex justify-center m-3" id="list-container" name="list-container">';
+
         foreach ($items as $item) {
-            echo "<li>
-                    <strong>Title:</strong> {$item['title']}<br>
-                    <strong>Description:</strong> {$item['description']}<br>
-                    <strong>Date:</strong> {$item['date']}
-                </li>";
+            echo '<ul class="bg-slate-200 rounded-md border border-slate-700 p-2 w-2/5">
+                <div class="flex p-1" id="top-container" name="top-container">
+                    <li class="flex-1 w-1/2">
+                        <input class="px-2" id="list-checkbox" name="list-checkbox" type="checkbox">
+                        <label class="text-gray-700 text-base font-semibold" for="list-checkbox">' . $item['title'] . '</label>
+                    </li>
+                    <li>' . $item['date'] . '</li>
+                </div>
+                <div class="flex p-1" id="bottom-container" name="bottom-container">
+                    <li class="flex-1 w-1/2">
+                        <p class="text-gray-700 text-sm font-normal" id="list-description" name="list-description">' . $item['description'] . '</p>
+                    </li>
+                    <li class="flex flex-col-reverse">
+                        <a href="#" id="edit-btn" name="edit-btn" class="flex-shrink-0 bg-slate-300 hover:bg-slate-400 border-slate-300 hover:border-slate-400 text-sm border-4 text-slate-700 py-1 px-2 rounded">Edit</a>
+                    </li>
+                </div>
+            </ul>';
         }
-        echo "</ul>
-        </body>
-        </html>";
+        
+                echo '</div>';
+        
+        
     }
 }
